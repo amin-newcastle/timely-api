8c81de83fd2c56d90de5da05c06a3de82ed9fdf6
Amin <muhammad.karim@engineering.digital.dwp.gov.uk>
2023-06-02 15:14:22 +0100

    feat: add route file

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route files
+const users = require('./server/routes/users');
+
 // Load env vars
 dotenv.config({ path: 'server/config/config.env' });
 
 const app = express();
+
+// Mount routers
+app.use('/api/v1/users', users);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
